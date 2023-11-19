b6f2bb00b99a0ab425fc83e1a00bd96edb793009
Mateus Nosse <80108925+MNosse@users.noreply.github.com>
2023-11-19 16:43:22 -0300

    adding logger and swagger

--- src/main/kotlin/br/com/udesc/ceavi/eso/ese/reviewservice/ReviewServiceApplication.kt
+++ src/main/kotlin/br/com/udesc/ceavi/eso/ese/reviewservice/ReviewServiceApplication.kt
@@ -1,11 +1,28 @@
 package br.com.udesc.ceavi.eso.ese.reviewservice
 
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 
 @SpringBootApplication
 class ReviewServiceApplication
 
 fun main(args: Array<String>) {
 	runApplication<ReviewServiceApplication>(*args)
+}
+
+@Configuration
+class OpenApiConfig() {
+	@Bean
+	fun usersMicroserviceOpenApi(): OpenAPI {
+		return OpenAPI()
+			.info(
+				Info().title("Review-Web-Service")
+					.description("A Review web service")
+					.version("1.0")
+			)
+	}
 }
